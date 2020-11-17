50d68f0eceaaa26622e9f5ef6f9706da6005a110
flis_ann <ann.flismark@gmail.com>
2020-11-17 08:45:08 +0100

    Adding a test for imperial calculation

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -1,11 +1,24 @@
 ///<reference types="Cypress" />
 
 describe('BMI Calculator', () => {
-  it('Calculates BMI in metric', () => {
+  beforeEach(() => {
     cy.visit('/');
+  })
+  
+  it('Calculates BMI in metric', () => {
+    cy.get('select#select-method').select('metric');
     cy.get('input#weight').type(90);
     cy.get('input#height').type(190);
     cy.get('button#calculate').click();
     cy.get('p#bmi-message').should('contain', 'Cats are the best');
   });
-}); 
+
+  it('Calculates BMI in imperial', () => {
+    cy.get('select#select-method').select('imperial');
+    cy.get('input#weight').type(198);
+    cy.get('input#height').type(74);
+    cy.get('button#calculate').click();
+    cy.get('p#bmi-message').should('contain', 'I love dogs');
+  });
+});
+ 
